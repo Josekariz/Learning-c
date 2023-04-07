5e6d71a16743196e879b04eae4b3f2152593172e
Joseph <sejokarizz@gmail.com>
2023-04-07 09:21:50 +0300

    print Cool on terminal in C

---
+++ hello.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+/**
+ * main - method to run
+ * ./hello - runs the code
+ * complier - gcc -o hello hello.c
+*/
+{
+    printf(" _ ");
+    printf("_ ");
+    printf("_ ");
+    printf("_\n");
+    printf("|\n");
+    printf("|\n");
+    printf("|\n");
+    printf("|");
+
+    printf("_ ");
+    printf("_ ");
+    printf("_ ");
+    printf("_\n");   
+    
+    
+    // this^^ code prints the letter C
+    printf(" _ ");
+    printf("_ ");
+    printf("_ ");
+    printf("_ \n");
+    printf("(       )\n");
+    printf("|       |\n");
+    printf("|       |\n");
+    printf("(");
+
+    printf("_ ");
+    printf("_ ");
+    printf("_ ");
+    printf("_"); 
+    printf(")\n");
+    
+    printf(" _ ");
+    printf("_ ");
+    printf("_ ");
+    printf("_ \n");
+    printf("(       )\n");
+    printf("|       |\n");
+    printf("|       |\n");
+    printf("(");
+
+    printf("_ ");
+    printf("_ ");
+    printf("_ ");
+    printf("_"); 
+    printf(")\n");
+    
+    
+    //this ^^ is the letter O
+    printf("\n");
+    printf("|\n");
+    printf("|\n");
+    printf("|\n");
+    printf("(");
+    printf("_ ");
+    printf("_ ");
+    printf("_ ");
+    printf("_\n");
+    // this ^^ is the letter L
+    
+    
+
+    return 0;
+}
