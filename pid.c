f2607576bcf235bd7b9b214458c1f56c4267a908
Joseph <sejokarizz@gmail.com>
2023-05-13 13:37:27 +0300

    get parent process id

--- pid.c
+++ pid.c
@@ -4,13 +4,16 @@
  * main - Entry point
  * getpid() - returns the process id that is running
  *
+ * getppid() - returns the process id that is parent to the current 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    pid_t my_pid;
+    pid_t my_pid, parentId;
 
     my_pid = getpid();
+    parentId = getppid();
     printf("This is my PID: %u\n", my_pid);
+    printf("This is my Parent process id: %u\n", parentId);
     return (0);
 }
