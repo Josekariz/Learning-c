6215031abecd57d7201c3ffff56835e1398cd9f0
Joseph <sejokarizz@gmail.com>
2023-05-13 14:19:51 +0300

    program that takes user input & displays  (mirror)

--- shel.c
+++ shel.c
@@ -1,12 +1,33 @@
 #include "main.h"
 
-int main(int ac, char **av)
+/* This program reads input from the user and mirrors it back to them. */
+int main()
 {
-    int i;
+    char *line = NULL;
+    ssize_t read;
+    size_t length = 0;
 
-    for (i = 1; i < ac; i++)
+    while (1)
     {
-        printf("This is what you entered\n %s \n", av[i]);
+        printf("Mirrortext:$ ");
+
+        /* Read the user input from stdin using the getline function. */
+        read = getline(&line, &length, stdin);
+
+        /* If the return value of getline is -1, it means there was an error or
+           the end-of-file (EOF) was reached, so we break out of the loop. */
+        if (read == -1)
+        {
+            break;
+        }
+
+        /* Print the mirrored text back to the user. */
+        printf("This is the mirror text: %s", line);
     }
+
+    /* Free the memory allocated for the input line. */
+    free(line);
+
+    /* Return 0 to indicate successful completion of the program. */
     return 0;
 }
